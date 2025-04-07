5ba2d8f4fa6ad02679fed934122f6e7f803866ee
Felixrds77 <felixcanarinho@gmail.com>
2025-04-07 09:50:40 -0300

    meses

--- exercicio01.py
+++ exercicio01.py
@@ -1,1 +1,28 @@
-print ("cuida")
+nome = int(input("diga um numero de 1 a 12: "))
+
+if nome ==1 :
+    print("janeiro")
+elif nome ==2:
+    print("feveriro")
+elif nome ==3 :
+    print("março")
+elif nome ==4:
+    print("abril")
+elif nome == 5:
+    print("maio")
+elif nome == 6:
+    print("junho")
+elif nome == 7:
+    print("julho")
+elif nome == 8:
+    print("agosto")
+elif nome ==9:
+    print("setembro")
+elif nome == 10:
+    print("outubro")
+elif nome == 11:
+    print("novembro")
+elif nome ==12:
+    print("dezembro")
+else:
+    print("valor invalido")
